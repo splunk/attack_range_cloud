aa4fac2603158b4de4cc5fa23d331b224566ffc8
bpatel <bpatel@splunk.com>
2021-02-26 14:59:26 -0800

    first draft

--- cloud_attack_range.py
+++ cloud_attack_range.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from modules import logger
+from modules import configuration
 from pathlib import Path
 from modules.TerraformController import TerraformController
 from modules.CustomConfigParser import CustomConfigParser
@@ -13,41 +14,8 @@ os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
 
 VERSION = 1
 
-
-if __name__ == "__main__":
-    # grab arguments
-    parser = argparse.ArgumentParser(description="starts a cloud attack range ready to collect attack data into splunk")
-    parser.add_argument("-a", "--action", required=False, choices=['build', 'destroy', 'simulate', 'stop', 'resume', 'dump'], default="",
-                        help="action to take on the range, defaults to \"build\", build/destroy/simulate/stop/resume allowed")
-    parser.add_argument("-st", "--simulation_technique", required=False, type=str, default="",
-                        help=" MITRE ATT&CK technique ID to simulate in the attack_range, example: T1098, requires action simulate")
-    parser.add_argument("-sf", "--simulation_file", required=False, type=str, default="",
-                        help="path to simulation file, e.g. leonidas/definitions/persistence/create_iam_group.yml")
-    parser.add_argument("-sv", "--simulation_vars", required=False, type=str, default="",
-                        help="comma separated list of simulation vars, --simulation_vars 'user=test, password=test'")
-    parser.add_argument("-f", "--force", required=False, default=False, action="store_true",
-                        help="directly run the attack without popup")
-    parser.add_argument("-dn", "--dump_name", required=False, default="",
-                        help="name for the dumped attack data")
-    parser.add_argument("-c", "--config", required=False, default="cloud_attack_range.conf",
-                        help="path to the configuration file of the attack range")
-    parser.add_argument("-lm", "--list_machines", required=False, default=False, action="store_true", help="prints out all available machines")
-    parser.add_argument("-v", "--version", default=False, action="store_true", required=False,
-                        help="shows current attack_range version")
-
-    # parse them
-    args = parser.parse_args()
-    ARG_VERSION = args.version
-    action = args.action
+def init(args):
     config = args.config
-    simulation_technique = args.simulation_technique
-    simulation_file = args.simulation_file
-    list_machines = args.list_machines
-    force = args.force
-    simulation_vars = args.simulation_vars
-    dump_name = args.dump_name
-
-
     print("""
 starting program loaded for B1 battle droid
           ||/__'`.
@@ -65,6 +33,8 @@ starting program loaded for B1 battle droid
             |//
            .'=`=.
     """)
+
+
 
     # parse config
     attack_range_config = Path(config)
@@ -86,50 +56,224 @@ starting program loaded for B1 battle droid
         log.info("version: {0}".format(VERSION))
         sys.exit(0)
 
-    # identfy not allowed argument combination
-    if action == "simulate" and (simulation_file == "" and simulation_technique == ""):
-        log.error("ERROR: action simulate need either flag --simulation_file or --simulation_technique")
-        sys.exit(1)
+    return TerraformController(config, log), config, log
 
-    if action == "dump" and dump_name == "":
-        log.error("ERROR: action dump need the flag --dump_name")
-        sys.exit(1)
+def configure(args):
+    configuration.new(args.config)
 
-    if action == "" and not list_machines:
-        log.error('ERROR: flag --action is needed.')
-        sys.exit(1)
-
-    if config['attack_range_password'] == 'I-l1ke-Attack-Range!':
-        log.error('ERROR: please change attack_range_password in attack_range.conf')
-        sys.exit(1)
-
-    if len(config['key_name']) > 20:
-        log.error('ERROR: your key_name is too long. Please create a shorter key_name. Maximum number of characters are 20.')
-        sys.exit(1)
-
-    controller = TerraformController(config, log)
-
-    if list_machines:
+def show(args):
+    controller, _, _ = init(args)
+    if args.machines:
         controller.list_machines()
-        sys.exit(0)
 
-    if action == 'build':
-        controller.build()
+def simulate(args):
+    controller, config, _ = init(args)
+    target = args.target
+    simulation_techniques = args.simulation_technique
+    simulation_atomics = args.simulation_atomics
+    # lets give CLI priority over config file for pre-configured techniques
+    if simulation_techniques:
+        pass
+    else:
+        simulation_techniques = config['art_run_techniques']
 
-    if action == 'destroy':
-        controller.destroy()
+    if not simulation_atomics:
+        simulation_atomics = 'no'
+    return controller.simulate(target, simulation_techniques, simulation_atomics)
 
-    if action == 'stop':
-        controller.stop()
-
-    if action == 'resume':
-        controller.resume()
-
-    if action == 'simulate':
-        controller.simulate(simulation_technique, simulation_file, force, simulation_vars)
-
-    if action == 'dump':
-        controller.dump(dump_name)
+def dump(args):
+    controller, _, _ = init(args)
+    controller.dump_attack_data(args.dump_name, args.last_sim)
 
 
-# rnfgre rtt ol C4G12VPX
+def replay(args):
+    controller, _, _ = init(args)
+    controller.replay_attack_data(args.dump_name, args.dump)
+
+
+def build(args):
+    controller, _, _ = init(args)
+    controller.build()
+
+
+def destroy(args):
+    controller, _, _ = init(args)
+    controller.destroy()
+
+
+def stop(args):
+    controller, _, _ = init(args)
+    controller.stop()
+
+
+def resume(args):
+    controller, _, _ = init(args)
+    controller.resume()
+
+
+def test(args):
+    controller, _, _ = init(args)
+    return controller.test(args.test_file)
+
+def main(args):
+    # grab arguments
+    parser = argparse.ArgumentParser(
+        description="Use `attack_range.py action -h` to get help with any Attack Range action")
+    parser.add_argument("-c", "--config", required=False, default="attack_range.conf",
+                        help="path to the configuration file of the attack range")
+    parser.add_argument("-v", "--version", default=False, action="version", version="version: {0}".format(VERSION),
+                        help="shows current attack_range version")
+    parser.set_defaults(func=lambda _: parser.print_help())  
+
+    actions_parser = parser.add_subparsers(title="Attack Range actions", dest="action")
+    configure_parser = actions_parser.add_parser("configure", help="configure a new attack range")
+    build_parser = actions_parser.add_parser("build", help="Builds attack range instances")
+    simulate_parser = actions_parser.add_parser("simulate", help="Simulates attack techniques")
+    destroy_parser = actions_parser.add_parser("destroy", help="destroy attack range instances")
+    stop_parser = actions_parser.add_parser("stop", help="stops attack range instances")
+    resume_parser = actions_parser.add_parser("resume", help="resumes previously stopped attack range instances")
+    show_parser = actions_parser.add_parser("show", help="list machines")
+    test_parser = actions_parser.add_parser("test")
+    dump_parser = actions_parser.add_parser("dump", help="dump locally logs from attack range instances")
+    replay_parser = actions_parser.add_parser("replay", help="replay dumps into the Splunk Enterprise server")
+
+    # Build arguments
+    build_parser.set_defaults(func=build)
+
+    # Destroy arguments
+    destroy_parser.set_defaults(func=destroy)
+
+    # Stop arguments
+    stop_parser.set_defaults(func=stop)
+
+    # Resume arguments
+    resume_parser.set_defaults(func=resume)
+
+    # Configure arguments
+    configure_parser.add_argument("-c", "--config", required=False, type=str, default='attack_range.conf',
+                                    help="provide path to write configuration to")
+    configure_parser.set_defaults(func=configure)
+
+    # Simulation arguments
+    simulate_parser.add_argument("-t", "--target", required=True,
+                                 help="target for attack simulation. Use the name of the aws EC2 name")
+    simulate_parser.add_argument("-st", "--simulation_technique", required=False, type=str, default="",
+                                 help="comma delimited list of MITRE ATT&CK technique ID to simulate in the "
+                                      "attack_range, example: T1117, T1118, requires --simulation flag")
+    simulate_parser.add_argument("-sa", "--simulation_atomics", required=False, type=str, default="",
+                                 help="specify dedicated Atomic Red Team atomics to simulate in the attack_range, "
+                                      "example: Regsvr32 remote COM scriptlet execution for T1117")
+    simulate_parser.set_defaults(func=simulate)
+
+    # Dump  Arguments
+    dump_parser.add_argument("-dn", "--dump_name", required=True,
+                             help="name for the dumped attack data")
+    dump_parser.add_argument("--last-sim", required=False, action='store_true',
+                             help="overrides dumps.yml time and dumps from the start of previous simulation")
+    dump_parser.set_defaults(func=dump)
+
+    # Replay Arguments
+    replay_parser.add_argument("-dn", "--dump_name", required=True,
+                               help="name for the dumped attack data")
+    replay_parser.add_argument("--dump", required=False,
+                        help="name of the dump as defined in attack_data/dumps.yml")
+    replay_parser.set_defaults(func=replay)
+
+    # Test Arguments
+    test_parser.add_argument("-tf", "--test_file", required=True,
+                             type=str, default="", help='test file for test command')
+    test_parser.set_defaults(func=test)
+
+    # Show arguments
+    show_parser.add_argument("-m", "--machines", required=False, default=False,
+                             action="store_true", help="prints out all available machines")
+    show_parser.set_defaults(func=show, machines=True)
+
+    # # parse them
+    args = parser.parse_args()
+    return args.func(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
+#     parser = argparse.ArgumentParser(description="starts a cloud attack range ready to collect attack data into splunk")
+#     parser.add_argument("-a", "--action", required=False, choices=['build', 'destroy', 'simulate', 'stop', 'resume', 'dump'], default="",
+#                         help="action to take on the range, defaults to \"build\", build/destroy/simulate/stop/resume allowed")
+#     parser.add_argument("-st", "--simulation_technique", required=False, type=str, default="",
+#                         help=" MITRE ATT&CK technique ID to simulate in the attack_range, example: T1098, requires action simulate")
+#     parser.add_argument("-sf", "--simulation_file", required=False, type=str, default="",
+#                         help="path to simulation file, e.g. leonidas/definitions/persistence/create_iam_group.yml")
+#     parser.add_argument("-sv", "--simulation_vars", required=False, type=str, default="",
+#                         help="comma separated list of simulation vars, --simulation_vars 'user=test, password=test'")
+#     parser.add_argument("-f", "--force", required=False, default=False, action="store_true",
+#                         help="directly run the attack without popup")
+#     parser.add_argument("-dn", "--dump_name", required=False, default="",
+#                         help="name for the dumped attack data")
+#     parser.add_argument("-c", "--config", required=False, default="cloud_attack_range.conf",
+#                         help="path to the configuration file of the attack range")
+#     parser.add_argument("-lm", "--list_machines", required=False, default=False, action="store_true", help="prints out all available machines")
+#     parser.add_argument("-v", "--version", default=False, action="store_true", required=False,
+#                         help="shows current attack_range version")
+
+#     # parse them
+#     args = parser.parse_args()
+#     ARG_VERSION = args.version
+#     action = args.action
+#     config = args.config
+#     simulation_technique = args.simulation_technique
+#     simulation_file = args.simulation_file
+#     list_machines = args.list_machines
+#     force = args.force
+#     simulation_vars = args.simulation_vars
+#     dump_name = args.dump_name
+
+
+#     # identfy not allowed argument combination
+#     if action == "simulate" and (simulation_file == "" and simulation_technique == ""):
+#         log.error("ERROR: action simulate need either flag --simulation_file or --simulation_technique")
+#         sys.exit(1)
+
+#     if action == "dump" and dump_name == "":
+#         log.error("ERROR: action dump need the flag --dump_name")
+#         sys.exit(1)
+
+#     if action == "" and not list_machines:
+#         log.error('ERROR: flag --action is needed.')
+#         sys.exit(1)
+
+#     if config['attack_range_password'] == 'I-l1ke-Attack-Range!':
+#         log.error('ERROR: please change attack_range_password in attack_range.conf')
+#         sys.exit(1)
+
+#     if len(config['key_name']) > 20:
+#         log.error('ERROR: your key_name is too long. Please create a shorter key_name. Maximum number of characters are 20.')
+#         sys.exit(1)
+
+#     controller = TerraformController(config, log)
+
+#     if list_machines:
+#         controller.list_machines()
+#         sys.exit(0)
+
+#     if action == 'build':
+#         controller.build()
+
+#     if action == 'destroy':
+#         controller.destroy()
+
+#     if action == 'stop':
+#         controller.stop()
+
+#     if action == 'resume':
+#         controller.resume()
+
+#     if action == 'simulate':
+#         controller.simulate(simulation_technique, simulation_file, force, simulation_vars)
+
+#     if action == 'dump':
+#         controller.dump(dump_name)
+
+
+# # rnfgre rtt ol C4G12VPX
--- modules/TerraformController.py
+++ modules/TerraformController.py
@@ -13,18 +13,39 @@ import glob
 import pathlib
 
 
-class TerraformController():
+class TerraformController(IEnvironmentController):
 
     def __init__(self, config, log):
-        self.config = config
-        self.log = log
+        super().__init__(config, log)
+        statefile = self.config['range_name'] + ".terraform.tfstate"
+        if self.config['cloud_provider'] == 'aws':
+            self.config["statepath"] = os.path.join(os.path.dirname(__file__), '../terraform/aws/state', statefile)
+        elif self.config['cloud_provider'] == 'azure':
+            self.config["statepath"] = os.path.join(os.path.dirname(__file__), '../terraform/azure/state', statefile)
+
+        self.config['splunk_es_app_version'] = re.findall(r'\d+', self.config['splunk_es_app'])[0]
+
         custom_dict = self.config.copy()
         variables = dict()
         variables['config'] = custom_dict
-        self.terraform = Terraform(working_dir='terraform',variables=variables)
+
+        if self.config['cloud_provider'] == 'aws':
+            self.terraform = Terraform(working_dir=os.path.join(os.path.dirname(__file__), '../terraform/aws'),variables=variables, parallelism=15 ,state=config["statepath"])
+        # elif self.config['cloud_provider'] == 'azure':
+        #     self.terraform = Terraform(working_dir=os.path.join(os.path.dirname(__file__), '../terraform/azure'),variables=variables, parallelism=15 ,state=config["statepath"])
+
+        #OLD CODE
+
+        # self.config = config
+        # self.log = log
+        # custom_dict = self.config.copy()
+        # variables = dict()
+        # variables['config'] = custom_dict
+        # self.terraform = Terraform(working_dir='terraform',variables=variables)
 
 
     def build(self):
+
         self.log.info("[action] > build\n")
         return_code, stdout, stderr = self.terraform.apply(capture_output='yes', skip_plan=True, no_color=IsNotFlagged)
         if not return_code:
@@ -37,23 +58,146 @@ class TerraformController():
 
 
     def destroy(self):
-        if self.config["kubernetes"]=="1":
-            kubernetes_service.delete_application(self.config, self.log)
+
         self.log.info("[action] > destroy\n")
-        return_code, stdout, stderr = self.terraform.destroy(capture_output='yes', no_color=IsNotFlagged)
-        self.log.info("attack_range has been destroy using terraform successfully")
+        return_code, stdout, stderr = self.terraform.destroy(
+            capture_output='yes', no_color=IsNotFlagged)
+        self.log.info("Destroyed with return code: " + str(return_code))
+        statepath = self.config["statepath"]
+        statebakpath = self.config["statepath"] + ".backup"
+        if os.path.exists(statepath) and return_code==0:
+            try:
+                os.remove(statepath)
+                os.remove(statebakpath)
+            except Exception as e:
+                self.log.error("not able to delete state file")
+        self.log.info(
+            "attack_range has been destroy using terraform successfully")
+
+        # if self.config["kubernetes"]=="1":
+        #     kubernetes_service.delete_application(self.config, self.log)
+        # self.log.info("[action] > destroy\n")
+        # return_code, stdout, stderr = self.terraform.destroy(capture_output='yes', no_color=IsNotFlagged)
+        # self.log.info("attack_range has been destroy using terraform successfully")
 
 
     def stop(self):
-        instances = aws_service.get_all_instances(self.config)
-        aws_service.change_ec2_state(instances, 'stopped', self.log)
+        if self.config['cloud_provider'] == 'aws':
+            instances = aws_service.get_all_instances(self.config)
+            aws_service.change_ec2_state(instances, 'stopped', self.log, self.config)
+        elif self.config['cloud_provider'] == 'azure':
+            azure_service.change_instance_state(self.config, 'stopped', self.log)
+
+        # instances = aws_service.get_all_instances(self.config)
+        # aws_service.change_ec2_state(instances, 'stopped', self.log)
 
 
     def resume(self):
-        instances = aws_service.get_all_instances(self.config)
-        aws_service.change_ec2_state(instances, 'running', self.log)
+        if self.config['cloud_provider'] == 'aws':
+            instances = aws_service.get_all_instances(self.config)
+            aws_service.change_ec2_state(instances, 'running', self.log, self.config)
+        elif self.config['cloud_provider'] == 'azure':
+            azure_service.change_instance_state(self.config, 'running', self.log)
+        # instances = aws_service.get_all_instances(self.config)
+        # aws_service.change_ec2_state(instances, 'running', self.log)
 
+    def test(self, test_file):
+        # read test file
+        test_file = self.load_file(test_file)
 
+        # build attack range
+        self.build()
+
+        epoch_time = str(int(time.time()))
+        folder_name = "attack_data_" + epoch_time
+        os.mkdir(os.path.join(os.path.dirname(__file__), '../attack_data/' + folder_name))
+
+        output = 'loaded attack data'
+
+        if self.config['update_escu_app'] == '1':
+            self.update_ESCU_app()
+
+        result_tests = []
+
+        for test in test_file['tests']:
+            result_test = {}
+            for attack_data in test['attack_data']:
+                url = attack_data['data']
+                r = requests.get(url, allow_redirects=True)
+                open(os.path.join(os.path.dirname(__file__), '../attack_data/' + folder_name + '/' + attack_data['file_name']), 'wb').write(r.content)
+
+                # Update timestamps before replay
+                if 'update_timestamp' in attack_data:
+                    if attack_data['update_timestamp'] == True:
+                        data_manipulation = DataManipulation()
+                        data_manipulation.manipulate_timestamp(folder_name + '/' + attack_data['file_name'], self.log, attack_data['sourcetype'], attack_data['source'])
+
+                self.replay_attack_data(folder_name, None, {'sourcetype': attack_data['sourcetype'], 'source': attack_data['source'], 'out': attack_data['file_name']})
+
+            self.log.info('Wait for 200 seconds')
+            time.sleep(200)
+
+            if 'baselines' in test:
+                results_baselines = []
+                for baseline_obj in test['baselines']:
+                    baseline_file_name = baseline_obj['file']
+                    baseline = self.load_file(os.path.join(os.path.dirname(__file__), '../../security_content/' + baseline_file_name))
+                    result_obj = dict()
+                    result_obj['baseline'] = baseline_obj['name']
+                    result_obj['baseline_file'] = baseline_obj['file']
+                    if self.config['cloud_provider'] == 'aws':
+                        instance = aws_service.get_instance_by_name(
+                            'ar-splunk-' + self.config['range_name'] + '-' + self.config['key_name'], self.config)
+                        if instance['State']['Name'] == 'running':
+                            result = splunk_sdk.test_baseline_search(instance['NetworkInterfaces'][0]['Association']['PublicIp'], str(self.config['attack_range_password']), baseline['search'], baseline_obj['pass_condition'], baseline['name'], baseline_obj['file'], baseline_obj['earliest_time'], baseline_obj['latest_time'], self.log)
+                            results_baselines.append(result)
+                        else:
+                            self.log.error('ERROR: Splunk server is not running.')
+                    elif self.config['cloud_provider'] == 'azure':
+                        instance = azure_service.get_instance(self.config, "ar-splunk-" + self.config['range_name'] + "-" + self.config['key_name'], self.log)
+                        if instance['vm_obj'].instance_view.statuses[1].display_status == "VM running":
+                            result = splunk_sdk.test_baseline_search(instance['public_ip'], str(self.config['attack_range_password']), baseline['search'], baseline_obj['pass_condition'], baseline['name'], baseline_obj['file'], baseline_obj['earliest_time'], baseline_obj['latest_time'], self.log)
+                            results_baselines.append(result)
+                result_test['baselines_result'] = results_baselines
+
+            detection_file_name = test['file']
+            detection = self.load_file(os.path.join(os.path.dirname(__file__), '../../security_content/detections/' + detection_file_name))
+            if self.config['cloud_provider'] == 'aws':
+                instance = aws_service.get_instance_by_name(
+                    'ar-splunk-' + self.config['range_name'] + '-' + self.config['key_name'], self.config)
+                if instance['State']['Name'] == 'running':
+                    result_detection = splunk_sdk.test_detection_search(instance['NetworkInterfaces'][0]['Association']['PublicIp'], str(self.config['attack_range_password']), detection['search'], test['pass_condition'], detection['name'], test['file'], test['earliest_time'], test['latest_time'], self.log)
+                    self.log.info('Running Detections now.')
+                else:
+                    self.log.error('ERROR: Splunk server is not running.')
+            elif self.config['cloud_provider'] == 'azure':
+                instance = azure_service.get_instance(self.config, "ar-splunk-" + self.config['range_name'] + "-" + self.config['key_name'], self.log)
+                if instance['vm_obj'].instance_view.statuses[1].display_status == "VM running":
+                    result_detection = splunk_sdk.test_detection_search(instance['public_ip'], str(self.config['attack_range_password']), detection['search'], test['pass_condition'], detection['name'], test['file'], test['earliest_time'], test['latest_time'], self.log)
+                    self.log.info('Running Detections now.')
+
+            result_detection['detection_name'] = test['name']
+            result_detection['detection_file'] = test['file']
+            result_test['detection_result'] = result_detection
+            result_tests.append(result_test)
+
+        self.log.info('Running Detections - Complete')
+
+        # destroy attack range
+        self.destroy()
+
+        return result_tests
+
+    def load_file(self, file_path):
+        with open(file_path, 'r', encoding="utf-8") as stream:
+            try:
+                file = list(yaml.safe_load_all(stream))[0]
+            except yaml.YAMLError as exc:
+                self.log.error(exc)
+                sys.exit("ERROR: reading {0}".format(file_path))
+        return file
+
+    # To be tested and refactored
     def simulate(self, simulation_technique, simulation_file, force, simulation_vars):
 
         # read definition files from Leonidas
